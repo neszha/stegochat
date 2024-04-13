2a8362c92050fbb7d2ef8b8434de61af2c8547bd
adislksn <adi.120140038@student.itera.ac.id>
2024-04-13 14:09:56 +0700

    feat(pages, components) : main, create, join, chatroom, MessageIn MessageOut

--- stegoview/src/App.vue
+++ stegoview/src/App.vue
@@ -1,85 +1,40 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
+  <div
+    class="offcanvas offcanvas-start"
+    data-bs-scroll="true"
+    data-bs-backdrop="false"
+    tabindex="-1"
+    id="offcanvasScrolling"
+    aria-labelledby="offcanvasScrollingLabel"
+  >
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Menu</h5>
+      <button
+        type="button"
+        class="btn-close text-reset"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <img
+        class="img-fluid d-block w-100"
+        src="@/assets/images/authentication/typeface-logo.png"
+        alt="First slide"
+      />
+      <ul class="list-group">
+        <RouterLink class="text-dark list-group-item" to="/">Home</RouterLink>
+        <RouterLink class="text-dark list-group-item" to="/join" aria-current="true"
+          >Join</RouterLink
+        >
+        <RouterLink class="text-dark list-group-item" to="/create">Create</RouterLink>
+        <RouterLink class="text-dark list-group-item" to="/chat">Chat</RouterLink>
+      </ul>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- stegoview/vite.config.ts
+++ stegoview/vite.config.ts
@@ -6,13 +6,25 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `
+  //       @import "@/assets/able-pro/settings/_bootstrap-variables.scss";
+  //       @import "@/assets/able-pro/settings/_color-variables.scss";
+  //       @import "@/assets/able-pro/settings/_theme-variables.scss";
+  //       @import "@/assets/able-pro/pages/_application.scss";
+  //       @import "@/assets/able-pro/pages/_chat.scss";
+  //       @import "@/assets/able-pro/layouts/_layouts.scss";
+  //       @import "@/assets/able-pro/components/_components-variable.scss";
+  //       `
+  //     }
+  //   }
+  // }
 })
