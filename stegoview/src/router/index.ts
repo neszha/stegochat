8613df9cb695864d59fa4f3749855ade8457b7bf
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2024-04-22 07:17:27 +0700

    feat(eslint): setup new eslint in stegoview

--- stegoview/src/router/index.ts
+++ stegoview/src/router/index.ts
@@ -1,29 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-      path: '/join',
-      name: 'join',
-      component: () => import('../views/JoinView.vue')
-    },
-    {
-      path: '/create',
-      name: 'create',
-      component: () => import('../views/CreateView.vue')
-    },
-    {
-      path: '/chat',
-      name: 'chat',
-      component: () => import('../views/ChatView.vue')
-    }
-  ]
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: async () => await import('../views/HomeView.vue')
+        },
+        {
+            path: '/join',
+            name: 'join',
+            component: async () => await import('../views/JoinView.vue')
+        },
+        {
+            path: '/create',
+            name: 'create',
+            component: async () => await import('../views/CreateView.vue')
+        },
+        {
+            path: '/chat',
+            name: 'chat',
+            component: async () => await import('../views/ChatView.vue')
+        }
+    ]
 })
 
 export default router
---
+++ stegoview/src/vue-shim.d.ts
@@ -0,0 +1,4 @@
+declare module '*.vue' {
+    import { VueElement } from 'vue'
+    export default VueElement
+}
