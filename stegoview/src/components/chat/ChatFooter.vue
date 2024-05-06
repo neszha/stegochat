60062aebad15b3ed10869e3439f2d7f8b266e19d
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2024-05-06 23:52:48 +0700

    feat(chat): update chat page view

---
+++ stegoview/src/components/chat/ChatFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card-footer px-3 py-2">
+        <textarea class="form-control border-0 shadow-none mb-2" placeholder="Type a Message" rows="1" style="max-height: 100px;"></textarea>
+        <div class="d-grid">
+            <button class="btn btn-primary d-flex align-items-center justify-content-center gap-2">
+                <i class="ti ti-send"></i>
+                <span>Send Message</span>
+            </button>
+        </div>
+    </div>
+</template>
---
+++ stegoview/src/components/chat/ChatHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card-header p-3">
+        <div class="d-flex align-items-center">
+            <ul class="list-inline me-auto mb-0">
+                <li class="list-inline-item align-bottom">
+                    <a href="#" class=" avtar avtar-s btn btn-link-secondary" type="button">
+                        <i class="ti ti-users f-18"></i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <div class="media align-items-center">
+                        <div class="media-body">
+                            <h6 class="mb-0">Private Channel</h6>
+                            <span class="text-sm text-muted">Fanesa Hadi Pramana</span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <ul class="list-inline ms-auto mb-0">
+                <li class="list-inline-item">
+                    <a href="#" class="avtar avtar-s btn-link-secondary">
+                        <i class="ti ti-logout f-18" style="rotate: 180deg;"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
